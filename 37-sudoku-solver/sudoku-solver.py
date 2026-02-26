684e747a059da3355a4cbe2c61828da162fbd1cc
Hardik Vala <163175577+Hardik336@users.noreply.github.com>
2026-02-26 22:34:38 +0530

    Time: 3308 ms (8.34%) | Memory: 12.6 MB (57.81%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.py
+++ 37-sudoku-solver/sudoku-solver.py
@@ -1,43 +1,41 @@
-class Solution(object):
+class Solution:
     def solveSudoku(self, board):
-
         rows = [set() for _ in range(9)]
         cols = [set() for _ in range(9)]
         boxes = [set() for _ in range(9)]
         empty = []
 
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == '.':
-                    empty.append((r, c))
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    empty.append((i, j))
                 else:
-                    num = board[r][c]
-                    rows[r].add(num)
-                    cols[c].add(num)
-                    boxes[(r//3)*3 + (c//3)].add(num)
+                    num = board[i][j]
+                    rows[i].add(num)
+                    cols[j].add(num)
+                    boxes[(i // 3) * 3 + (j // 3)].add(num)
 
         def backtrack(index):
             if index == len(empty):
                 return True
 
             r, c = empty[index]
-            box_id = (r//3)*3 + (c//3)
+            b = (r // 3) * 3 + (c // 3)
 
-            for num in "123456789":
-                if num not in rows[r] and num not in cols[c] and num not in boxes[box_id]:
-                    
+            for num in map(str, range(1, 10)):
+                if num not in rows[r] and num not in cols[c] and num not in boxes[b]:
                     board[r][c] = num
                     rows[r].add(num)
                     cols[c].add(num)
-                    boxes[box_id].add(num)
+                    boxes[b].add(num)
 
                     if backtrack(index + 1):
                         return True
 
-                    board[r][c] = '.'
+                    board[r][c] = "."
                     rows[r].remove(num)
                     cols[c].remove(num)
-                    boxes[box_id].remove(num)
+                    boxes[b].remove(num)
 
             return False
 
